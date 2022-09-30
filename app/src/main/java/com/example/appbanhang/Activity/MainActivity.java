0fe9eb4ba0d6cacfa90ed4452786f926110f05d9
DESKTOP-TO7M4DE\luan <luankyru@gmail.com>
2022-09-30 09:18:29 +0700

    add project

--- app/src/main/java/com/example/appbanhang/Activity/MainActivity.java
+++ app/src/main/java/com/example/appbanhang/Activity/MainActivity.java
@@ -61,6 +61,7 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity  {
+    //testgithub
     public static final int Fragmenthome = 0;
     public static final int Fragmentsanpham = 1;
     public static final int Fragmentgiohang = 2;
